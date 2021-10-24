df52d284141d1f7cf4e75a9239b2c712b6eab054
Drew Thomas <cdthomas.92@gmail.com>
2021-10-25 08:25:11 +1100

    First pass at stderr and exit code expectations

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,9 @@ struct Test {
     name: String,
     #[serde(rename = "in")]
     input: String,
-    out: String,
+    out: Option<String>,
+    err: Option<String>,
+    exit_code: Option<i32>,
 }
 
 struct Failure {
@@ -39,13 +41,15 @@ impl fmt::Display for Failure {
     }
 }
 
-struct Expectation {
-    expected: String,
-    actual: String,
+struct Expectation<T> {
+    expected: T,
+    actual: T,
 }
 
 enum FailedExpectation {
-    StdOut(Expectation),
+    StdOut(Expectation<String>),
+    StdErr(Expectation<String>),
+    ExitCode(Expectation<i32>),
 }
 
 impl fmt::Display for FailedExpectation {
@@ -67,24 +71,52 @@ impl fmt::Display for FailedExpectation {
                     Colour::Red.paint(&expectation.actual)
                 )
             }
+            FailedExpectation::StdErr(ref expectation) => {
+                write!(
+                    f,
+                    "    Unexpected output on stderr.\n\
+                    \n\
+                    \x20   Expected:\n\
+                    \n\
+                    \x20     {}\n\
+                    \n\
+                    \x20   Received:\n\
+                    \n\
+                    \x20     {}\n",
+                    Colour::Green.paint(&expectation.expected),
+                    Colour::Red.paint(&expectation.actual)
+                )
+            }
+            FailedExpectation::ExitCode(ref expectation) => {
+                write!(
+                    f,
+                    "    Unexpected exit code.\n\
+                    \n\
+                    \x20   Expected: {}\n\
+                    \n\
+                    \x20   Received: {}\n\n",
+                    Colour::Green.paint(expectation.expected.to_string()),
+                    Colour::Red.paint(expectation.actual.to_string())
+                )
+            }
         }
     }
 }
 
 #[derive(Debug)]
-struct Summary {
-    passed_count: usize,
-    failed_count: usize,
+struct TestCounts {
+    passed: usize,
+    failed: usize,
 }
 
-impl fmt::Display for Summary {
+impl fmt::Display for TestCounts {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let label_text = Style::new().bold().paint("Tests:");
-        let passed_text = Colour::Green.paint(format!("{} passed", self.passed_count));
-        let failed_text = Colour::Red.paint(format!("{} failed", self.failed_count));
-        let total_text = format!("{} total", self.passed_count + self.failed_count);
+        let passed_text = Colour::Green.paint(format!("{} passed", self.passed));
+        let failed_text = Colour::Red.paint(format!("{} failed", self.failed));
+        let total_text = format!("{} total", self.passed + self.failed);
 
-        if self.failed_count > 0 {
+        if self.failed > 0 {
             writeln!(
                 f,
                 "{} {}, {}, {}",
@@ -109,6 +141,8 @@ pub enum CliError {
 
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error: ")?;
+
         match *self {
             CliError::Io(ref err) => err.fmt(f),
             CliError::Yaml(ref err) => err.fmt(f),
@@ -137,27 +171,27 @@ impl From<string::FromUtf8Error> for CliError {
 
 // TODO:
 // - Add tests
-// - Add expecting stderr and error code
 // - Before/after each/all hooks
 // - Verify that test names are unique
+// - Verify that expectations are valid
 
 pub fn run(filename: String) -> Result<TestState, CliError> {
     let tests = parse(&filename)?;
 
-    let mut summary = Summary {
-        passed_count: 0,
-        failed_count: 0,
+    let mut test_counts = TestCounts {
+        passed: 0,
+        failed: 0,
     };
 
     let mut failures: Vec<Failure> = Vec::new();
 
     for test in tests {
-        run_test(test, &mut summary, &mut failures)?;
+        run_test(test, &mut test_counts, &mut failures)?;
     }
 
-    report_summary(&summary, &failures);
+    report_summary(&test_counts, &failures);
 
-    match summary.failed_count {
+    match test_counts.failed {
         0 => Ok(TestState::Passed),
         _ => Ok(TestState::Failed),
     }
@@ -172,27 +206,58 @@ fn parse(filename: &str) -> Result<Vec<Test>, CliError> {
 
 fn run_test(
     test: Test,
-    summary: &mut Summary,
+    test_counts: &mut TestCounts,
     failures: &mut Vec<Failure>,
 ) -> Result<(), CliError> {
     let output = Command::new("bash").arg("-c").arg(&test.input).output()?;
 
+    let mut failed_expectations: Vec<FailedExpectation> = Vec::new();
+
     let stdout = String::from_utf8(output.stdout)?;
 
-    if stdout.eq(&test.out) {
+    if let Some(expected_out) = test.out {
+        if stdout.ne(&expected_out) {
+            failed_expectations.push(FailedExpectation::StdOut(Expectation {
+                actual: stdout,
+                expected: expected_out,
+            }))
+        }
+    }
+
+    let stderr = String::from_utf8(output.stderr)?;
+
+    if let Some(expected_err) = test.err {
+        if stderr.ne(&expected_err) {
+            failed_expectations.push(FailedExpectation::StdErr(Expectation {
+                actual: stderr,
+                expected: expected_err,
+            }))
+        }
+    }
+
+    if let Some(expected_exit) = test.exit_code {
+        // TODO: remove unwrap
+        let actual_exit_code = output.status.code().unwrap();
+
+        if expected_exit != actual_exit_code {
+            failed_expectations.push(FailedExpectation::ExitCode(Expectation {
+                actual: actual_exit_code,
+                expected: expected_exit,
+            }))
+        }
+    }
+
+    if failed_expectations.len() == 0 {
         report_test_passed();
-        summary.passed_count += 1;
+        test_counts.passed += 1;
     } else {
         report_test_failed();
-        summary.failed_count += 1;
+        test_counts.failed += 1;
 
         failures.push(Failure {
             name: test.name,
-            failure_number: summary.failed_count,
-            failed_expectations: vec![FailedExpectation::StdOut(Expectation {
-                actual: stdout,
-                expected: test.out,
-            })],
+            failure_number: test_counts.failed,
+            failed_expectations,
         });
     }
 
@@ -207,8 +272,8 @@ fn report_test_failed() {
     print!("{}", Colour::Red.paint("F"));
 }
 
-fn report_summary(summary: &Summary, failures: &Vec<Failure>) {
-    print!("\n\n{}", summary);
+fn report_summary(test_counts: &TestCounts, failures: &Vec<Failure>) {
+    print!("\n\n{}", test_counts);
 
     if failures.len() > 0 {
         print!("\n{}\n\n", Style::new().bold().paint("Failures:"));
